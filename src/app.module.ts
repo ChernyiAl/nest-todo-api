6b9a9fb98bc368a1ae34af4690e42cd56d86ba26
Alex <chernyialeksandr@gmail.com>
2021-11-16 13:04:00 +0200

    create table in db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoModule } from './modules/todo/todo.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), TodoModule],
